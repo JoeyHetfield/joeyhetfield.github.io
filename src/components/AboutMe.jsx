0c82deb37f3476df291cd9e541eb7919855b6377
Joey Hetfield <joeydefenix@hotmail.com>
2023-02-09 07:30:16 -0300

    Mais uma tentativa de corrigir o erro

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../css/AboutMe.css';
-import profilepicture from '../profilepicture.jpg';
+import profilePicture from '../image/profilepicture.jpg';
 
 function AboutMe() {
   const [language, setLanguage] = useState('en');
@@ -8,7 +8,7 @@ function AboutMe() {
   return (
     <section>
       <h2>{language === 'en' ? 'About Me' : 'Sobre Mim'}</h2>
-      <img src={profilepicture} alt="Your name" />
+      <img src={profilePicture} alt="Your name" />
       <p>
         {language === 'en'
           ? 'I am a web development student from Brazil, currently 26 years old, and I am in the process of transitioning my career to technology. My goal is to secure my first job as a developer I have experience in HTML, CSS, JavaScript, JS6, Jest, React, and Redux, and I am currently learning about React Hooks. I am passionate about creating clean, efficient, and user-friendly web applications and I am constantly looking to improve my skills and learn new technologies Feel free to take a look at my projects to see my work. I have attempted to demonstrate my proficiency in various web development technologies. If you have any questions or feedback, please do not hesitate to reach out to me.'
